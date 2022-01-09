946c570b24dd5295143c0cddf3751120c98cfb8f
sangjin-hash <lsj9873@ajou.ac.kr>
2022-01-09 18:51:28 +0900

    Todo : Error 수정

--- app/src/main/java/com/example/toyapp/screen/RegisterFragment.kt
+++ app/src/main/java/com/example/toyapp/screen/RegisterFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.example.toyapp.R
 import com.example.toyapp.viewModel.RegisterViewModel
 import com.example.toyapp.databinding.FragmentRegisterBinding
@@ -31,8 +33,34 @@ class RegisterFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
         binding.registerViewModel = viewModel
 
-        // TODO("Observer 넣기")
+        viewModel.registerState.observe(viewLifecycleOwner, Observer<Int>{  tag ->
+            when(tag){
+                1 -> {
+                    binding.tvEmailValid.text = "아이디가 비어 있습니다"
+                    binding.tvPasswordValid.text = "비밀번호가 비어 있습니다"
+                }
+                2 -> {
+                    binding.tvEmailValid.text = "아이디가 비어 있습니다"
+                    binding.tvPasswordValid.text = ""
+                }
+                3 -> {
+                    binding.tvEmailValid.text = ""
+                    binding.tvPasswordValid.text = "비밀번호가 비어 있습니다"
+                }
+                4 -> {
+                    binding.tvEmailValid.text = "중복된 아이디입니다."
+                    binding.tvPasswordValid.text = ""
+                }
+                5 -> {
+                    binding.tvEmailValid.text = ""
+                    binding.tvPasswordValid.text = ""
 
+                    val action = RegisterFragmentDirections.actionRegisterFragmentToLoginFragment()
+                    findNavController().navigate(action)
+                }
+            }
+
+        })
 
         return binding.root
     }
